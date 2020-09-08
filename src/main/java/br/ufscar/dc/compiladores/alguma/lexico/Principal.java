087a1c0b15889fb00df2a1c11968df11a3ef06d8
hidekisrn <sergionisikava@gmail.com>
2020-09-08 11:17:35 -0300

    Salvando output em .txt

--- src/main/java/br/ufscar/dc/compiladores/alguma/lexico/Principal.java
+++ src/main/java/br/ufscar/dc/compiladores/alguma/lexico/Principal.java
@@ -1,6 +1,9 @@
 package br.ufscar.dc.compiladores.alguma.lexico;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.Token;
@@ -8,13 +11,14 @@ import org.antlr.v4.runtime.misc.ParseCancellationException;
 
 public class Principal {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        PrintStream out = new PrintStream(new FileOutputStream("/home/hidekisrn/output.txt"));
+        System.setOut(out);
         try {
             // args[0] é o primeiro argumento da linha de comando
             CharStream cs = CharStreams.fromFileName(args[0]);
             AlgumaLexer lex = new AlgumaLexer(cs);
             Token t = null;
-           System.out.println(); 
             while ((t = lex.nextToken()).getType() != Token.EOF) {
                 System.out.println("<" + "'" + t.getText() + "'" + "," + AlgumaLexer.VOCABULARY.getDisplayName(t.getType()) + ">");
             }
