b1d31f6c569acabcfa2a8a5f62fcf7815d58ce31
hidekisrn <sergionisikava@gmail.com>
2020-09-08 11:19:18 -0300

    Modificado o path do FileOutputStream

--- src/main/java/br/ufscar/dc/compiladores/alguma/lexico/Principal.java
+++ src/main/java/br/ufscar/dc/compiladores/alguma/lexico/Principal.java
@@ -12,7 +12,7 @@ import org.antlr.v4.runtime.misc.ParseCancellationException;
 public class Principal {
 
     public static void main(String[] args) throws FileNotFoundException {
-        PrintStream out = new PrintStream(new FileOutputStream("/home/hidekisrn/output.txt"));
+        PrintStream out = new PrintStream(new FileOutputStream("output.txt"));
         System.setOut(out);
         try {
             // args[0] é o primeiro argumento da linha de comando
